a021ee66ffa4e8c4ba2353e7de75620b123978f6
Giovanni Garufi <giovanni.garufi@fwr.it>
2015-05-12 23:34:08 +0200

    Polished code

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ var spawn = require('child_process').spawn
 var ProgressBar = require('progress')
 var exec = require('child_process').exec
 var fs = require('fs');
-var fmt = require('./dd-fmt.js');
-// var temp = require('temp').track()
+var fmt = require('./dd-fmt.js')
 
-
-if(process.getuid() != 0) throw 'You must run dd-node as root'
+if (process.getuid() != 0) {
+  console.log('You must run dd-node as root')
+  process.exit(0)
+}
 
 var args = process.argv
 
@@ -15,7 +16,6 @@ var inf = args[3]
 var of = args[4]
 
 var buf = []
-
 var inputFile = inf.split('=').pop()
 
 fs.stat(inputFile, function(err, stats) {
@@ -24,10 +24,10 @@ fs.stat(inputFile, function(err, stats) {
 
   var size = stats.size || Number.MAX_VALUE
   var oldTotal = 0
-
+  var totalTransferredBytes = 0
+  var progress = 0
 
   var dd = spawn('dd', [bs, inf, of], { 'stdio' : 'pipe' })
-
   dd.stderr.pipe(fmt)
 
   var bar = new ProgressBar('  transfering [:bar] :percent elapsed: :elapsed sec', {
@@ -42,8 +42,8 @@ fs.stat(inputFile, function(err, stats) {
   }, 1000)
 
   fmt.on('data', function(data) {
-    var totalTransferredBytes = parseInt(data.toString())
-    var progress = totalTransferredBytes - oldTotal
+    totalTransferredBytes = parseInt(data.toString())
+    progress = totalTransferredBytes - oldTotal
     bar.tick(progress)
     oldTotal = totalTransferredBytes
   })
--- dd-fmt.js
+++ dd-fmt.js
@@ -3,20 +3,18 @@ var _ = require('lodash')
 
 var filter = through2(function (chunk, enc, callback) {
   var self = this
-
   var buf = chunk.toString()
   var lines = buf.split('\n')
   var capture = /([\d]+) bytes/
 
-  var updates = _.compact(_.map(lines, function(line) {
+  var updates = _.compact(_.map(lines, function (line) {
     var match = capture.exec(line);
-
     if (match) {
       return match[1]
     }
   }))
 
-  _.forEach(updates, function(update) {
+  _.forEach(updates, function (update) {
     self.push(update);
   })
 
