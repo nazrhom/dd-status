51dce565bb899ab72cdadb6b4857b5642a9984eb
Giovanni Garufi <giovanni.garufi@fwr.it>
2015-05-18 18:11:48 +0200

    Add better arguments handling

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
-var spawn = require('child_process').spawn
+var _ = require('lodash')
 var ProgressBar = require('progress')
 var exec = require('child_process').exec
+var spawn = require('child_process').spawn
 var fs = require('fs')
 var fmtProgress = require('./dd-fmt.js').Progress
 var fmtError = require('./dd-fmt.js').Error
 
+
 if (process.getuid() != 0) {
   console.log('You must run dd-node as root')
-  process.exit(0)
+  process.exit(1)
 }
 
-var args = process.argv
-
-var bs = args[2]
-var inf = args[3]
-var of = args[4]
-
+var args = process.argv.slice(2)
 var buf = []
-var inputFile = inf.split('=').pop()
+var inputFile = findInputFile(args).split('=').pop()
 
 fs.stat(inputFile, function(err, stats) {
 
   if (err) stats = {}
 
   var size = stats.size || Number.MAX_VALUE
-  var oldTotal = 0
   var totalTransferredBytes = 0
+  var oldTotal = 0
   var progress = 0
 
-  var dd = spawn('dd', [bs, inf, of], { 'stdio' : 'pipe' })
+  var dd = spawn('dd', args, { 'stdio' : 'pipe' })
   dd.stderr.pipe(fmtProgress)
   dd.stderr.pipe(fmtError)
 
@@ -58,7 +55,14 @@ fs.stat(inputFile, function(err, stats) {
     if (dd.exitCode !== 0) {
       console.log('\n')
       console.log(data.toString())
-      process.exit(0)
+      process.exit(1)
     }
   })
 })
+
+
+function findInputFile(args) {
+  return _.find(args, function(arg) {
+    return arg.match(/if=[.]+/)
+  }) || ''
+}
